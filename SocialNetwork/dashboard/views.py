26bfc2717c1d2062b1ac613dc2cdbb8c10f0bf3c
Nitu <nitus22499@gmail.com>
2020-11-19 20:53:44 +0530

    comment delete n like with ajax with a bug

--- SocialNetwork/dashboard/views.py
+++ SocialNetwork/dashboard/views.py
@@ -7,6 +7,10 @@ from django.views.generic import CreateView, FormView, RedirectView,TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
 from .models import *
 from .forms import UserSignUpForm
+from .forms import EditProfileForm
+from post.models import *
+from django.forms.models import model_to_dict
+from post.views import *
 
 # Create your views here.
 
@@ -27,18 +31,71 @@ class RegisterView(CreateView):
         user.save()
         login(self.request, user)
         return redirect('post:home')
+
+        return kwargs
+
+
  
 class Logout(LogoutView):
     pass
 
-class ProfileView(TemplateView):
+class ProfileView(CreateView):
     template_name = 'dashboard/profile-view.html'
+    model=Post
+    def get_context_data(self, **kwargs):
+        user=self.request.user
+        posts=Post.objects.filter(user=user)
+        kwargs['posts']=posts
+        return kwargs
+
+class Edit_Profile(FormView):
+    model = User
+    form_class = EditProfileForm
+    template_name = 'dashboard/edit_profile.html'
+    success_url = reverse_lazy('dashboard:profile')
+    
+    def get_object(self):
+        """Check if data already exists"""
+        try:
+            self.object = User.objects.get(username= self.request.user)
+            print(self.object)
+            return self.object
+        except:
+            return None
+
+    def get_initial(self):
+        """Pre-fill the form if data exists"""
+        obj = self.get_object()
+        print(obj)
+        if obj is not None:
+            initial_data = model_to_dict(obj)
+            print(initial_data)
+            initial_data.update(model_to_dict(obj))
+            print(initial_data)
+            return initial_data
+        else:
+            return super().get_initial()
+
+    def form_valid(self, form):
+        """Save to the database. If data exists, update else create new record"""
+        print(self.object)
+        User.objects.filter(username=self.object).update(
+            first_name = form.cleaned_data['first_name'],
+            last_name = form.cleaned_data['last_name'],
+            email = form.cleaned_data['email'],
+            gender = form.cleaned_data['gender'],
+            date_of_birth = form.cleaned_data['date_of_birth'],
+        )
+        messages.success(self.request, 'Edited successfully')
+        return super().form_valid(form)
+
 
     
-def searchUser(request,searchfriend):
-    user = User.objects.filter(username=searchfriend)
-    return render(request,'post/home.html',{'user':user})
-
-# def home(request):
-#     return render(request,'dashboard/home.html',{})
+def searchUser(request):
+    
+    if request.method == 'GET': # this will be GET now      
+        searchfriend =  request.GET.get('searchfriend') # do some research what it does       
+        user = User.objects.filter(username=searchfriend)
+        print(user)
+        return render(request,'dashboard/profile-view.html',{'user':user})
 
--- SocialNetwork/post/views.py
+++ SocialNetwork/post/views.py
@@ -7,7 +7,51 @@ from dashboard.models import User
 from .forms import PostForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 # Create your views here.
+# ml
+# import numpy as np
+# import pandas 
+from sklearn.externals import joblib
+# endml
+
+
+
+
+# def _get_profane_prob(prob):
+#     return prob[1]
+
+# def preprocess(text):
+#     list_text = list(text.split())
+#     print(list_text)
+#     for word in list_text:
+#         predict(word)
+#     return
+
+def predict(text):
+#     list_text = list(text.split())
+# #     print(list_text)
+#     for text in list_text:
+# #         predict(word)
+    vectorizer = joblib.load('vectorizer.joblib')
+    model = joblib.load('model.joblib')
+
+    result = model.predict(vectorizer.transform(text))
+    if result == 1:
+        print('it contains vulgar words', text )
+        return result
+    else:
+        print('It is normal message', text)
+        return result
+        
+            
+# def predict_prob(texts):
+#     return np.apply_along_axis(_get_profane_prob, 1, model.predict_proba(vectorizer.transform(texts)))
+
+
 
 class HomeView(CreateView):
     model = Post
@@ -58,10 +102,20 @@ def like_unlike_post(request):
 
     return redirect('post:home')
 
+
 def create_comment(request, post_id=None):
+    counter=0
     if request.method == "POST":
         post = Post.objects.get(id=post_id)
         comment = post.comments.create(user=request.user, content=request.POST.get('content'))
+        print(comment.content)
+
+        val=predict([comment.content])
+        print(val[0])
+        if val==1:
+            messages.warning(request, 'Warning: You have used abusive word. Please maintain Decorum. Otherwise user will be blocked')
+            counter=counter+1
+            print(counter)
         return redirect(reverse_lazy('post:home'))
     else:
         return redirect(reverse_lazy('post:home'))
@@ -79,3 +133,15 @@ class PostDeleteView(LoginRequiredMixin, DeleteView):
         if not obj.user == self.request.user:
             messages.warning(self.request, 'You need to be the author of the post in order to delete it')
         return obj.delete()
+
+
+class CommentDeleteView(LoginRequiredMixin, DeleteView):
+    model = Comment
+    template_name = 'dashboard/home.html'
+    success_url = reverse_lazy('post:home')
+
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        print(pk)
+        Comment.objects.filter(id=pk).delete()
+        
